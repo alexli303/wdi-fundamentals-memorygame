c827644363085d69cbc0254264a1efb55683cacd
Alex Li <alexli@indiana.edu>
2017-06-20 15:07:38 -0600

    basic functionality working

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,71 +1,75 @@
- //console.log("Up and running!");
-/*
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
-*/
-
-//arrays of cards
 var cards = [
-  {
-    rank:'queen',
-    suit:'hearts',
-    cardImage:'images/queen-of-hearts.png'
-  },
-  {
-    rank:'queen',
-    suit:'diamonds',
-    cardImage:"images/queen-of-diamonds.png"
-  },
-  {
-    rank:'king',
-    suit:'hearts',
-    cardImage:"images/king-of-hearts.png"
-  },
-  {
-    rank:'king',
-    suit:'diamonds',
-    cardImage:"images/king-of-diamonds.png"
-  }
-
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
 ];
+
 var cardsInPlay = [];
+var gameBoard = document.getElementById('game-board');
+var resetButtonClicked = document.getElementById('reset');
 
-var checkForMatch = function(){
-  if (cardsInPlay[0] === cardsInPlay[1])
-  {
-     alert("You found a match!");
-  }
-  else
-  {
-    alert("Sorry, try again.");
-  }
+// Create a function to check for a match
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+	}
 }
 
-//function
-var flipCard = function(cardId){
-/*
-  var cardOne = cards[0];
-  cardsInPlay.push(cardOne);
-  console.log("User flipped " + cardOne);
+// Create a function that stores actions taken when users flips a card
+var flipCard = function() {
 
-  var cardTwo = cards[2];
-  cardsInPlay.push(cardTwo);
-  console.log("User flipped " + cardTwo);
-*/
-  console.log("User flipped " + cards[cardId].rank);
-  console.log("User flipped " + cards[cardId].cardImage);
-  console.log("User flipped " + cards[cardId].suit);
-  cardsInPlay.push(cards[cardId].rank);
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 
-  if (cardsInPlay.length === 2)
-  {
-    checkForMatch();
-  }
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	cardsInPlay.push(cards[cardId].rank);
+
+	checkForMatch();
+
 }
-flipCard(0);
 
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		gameBoard.appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+var resetButton = resetButtonClicked;
+resetButton.addEventListener('click', resetBoard);
+
+var resetBoard = function () {
+	gameBoard.refresh();
+  cardsInPlay = [];
+	createBoard();
+}
