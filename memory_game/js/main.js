f23794f9d2059c648b03adbc60c8cd76a6a83a0a
Alex Li <alexli@indiana.edu>
2017-06-20 12:01:06 -0600

    images

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,18 +8,33 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour);
 */
 
-var cards = ["queen", "queen", "king", "king"];
+//arrays of cards
+var cards = [
+  {
+    rank:'queen',
+    suit:'hearts',
+    cardImage:'images/queen-of-hearts.png'
+  },
+  {
+    rank:'queen',
+    suit:'diamonds',
+    cardImage:"images/queen-of-diamonds.png"
+  },
+  {
+    rank:'king',
+    suit:'hearts',
+    cardImage:"images/king-of-hearts.png"
+  },
+  {
+    rank:'king',
+    suit:'diamonds',
+    cardImage:"images/king-of-diamonds.png"
+  }
+
+];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2)
-{
+var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1])
   {
      alert("You found a match!");
@@ -29,3 +44,28 @@ if (cardsInPlay.length === 2)
     alert("Sorry, try again.");
   }
 }
+
+//function
+var flipCard = function(cardId){
+/*
+  var cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  console.log("User flipped " + cardOne);
+
+  var cardTwo = cards[2];
+  cardsInPlay.push(cardTwo);
+  console.log("User flipped " + cardTwo);
+*/
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].cardImage);
+  console.log("User flipped " + cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
+
+  if (cardsInPlay.length === 2)
+  {
+    checkForMatch();
+  }
+}
+flipCard(0);
+
+flipCard(2);
